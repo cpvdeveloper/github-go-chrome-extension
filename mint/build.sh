cbdc47eb3377f97aacd1dd6c0bdedcef9f613925
Chris Vibert <cp.vibert@gmail.com>
2021-04-12 20:19:08 +0700

    Use new repo icon

--- mint/build.sh
+++ mint/build.sh
@@ -9,10 +9,15 @@ copy_background() {
     cp ./js/background.js ./dist
 }
 
+copy_icon() {
+    cp icon.png ./dist
+}
+
 replace_manifest() {
     cp manifest.json ./dist
 }
 
 build
 copy_background
+copy_icon
 replace_manifest
