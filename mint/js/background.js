c68197331b2d43b963a75ea81424012efd35edfb
cpv123 <cp.vibert@gmail.com>
2021-04-28 09:17:02 +0700

    Update background to behave like service worker for V3

--- mint/js/background.js
+++ mint/js/background.js
@@ -2,9 +2,6 @@ const BASE_URL = "https://github.com";
 const STORAGE_PROFILE = "githubProfile";
 const STORAGE_REPOS = "githubRepos";
 
-let storedGithubProfile;
-let storedRepos = [];
-
 const reposToSuggestions = (repos) => {
   return repos.map((repoName) => ({
     content: repoName,
@@ -12,36 +9,30 @@ const reposToSuggestions = (repos) => {
   }));
 };
 
-chrome.omnibox.onInputStarted.addListener(() => {
-  chrome.storage.sync.get([STORAGE_PROFILE, STORAGE_REPOS], (storage) => {
-    if (storage.githubProfile) {
-      storedGithubProfile = storage.githubProfile;
-
-      if (storage.githubRepos) {
-        storedRepos = storage.githubRepos;
-      }
-    }
-  });
-});
-
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  if (!text || text.length === 0) {
-    const allSuggestions = reposToSuggestions(storedRepos);
-    return suggest(allSuggestions);
-  }
-  const filteredRepos = storedRepos.filter((repoName) => {
-    const searchTerm = text.toLowerCase();
-    const doesMatch = repoName.toLowerCase().includes(searchTerm);
-    return doesMatch;
-  });
-  const suggestions = reposToSuggestions(filteredRepos);
-  return suggest(suggestions);
+  chrome.storage.sync.get([STORAGE_REPOS], (storage) => {
+    const storedRepos = storage[STORAGE_REPOS];
+    if (!text || text.length === 0) {
+      const allSuggestions = reposToSuggestions(storedRepos);
+      return suggest(allSuggestions);
+    }
+    const filteredRepos = storedRepos.filter((repoName) => {
+      const searchTerm = text.toLowerCase();
+      const doesMatch = repoName.toLowerCase().includes(searchTerm);
+      return doesMatch;
+    });
+    const suggestions = reposToSuggestions(filteredRepos);
+    return suggest(suggestions);
+  })
 });
 
 chrome.omnibox.onInputEntered.addListener((text) => {
-  let githubUrl = `${BASE_URL}/${storedGithubProfile}`;
-  if (text) {
-    githubUrl += `/${text}`;
-  }
-  chrome.tabs.update({ url: githubUrl })
+  chrome.storage.sync.get([STORAGE_PROFILE], (storage) => {
+    const storedGithubProfile = storage[STORAGE_PROFILE];
+    let githubUrl = `${BASE_URL}/${storedGithubProfile}`;
+    if (text) {
+      githubUrl += `/${text}`;
+    }
+    chrome.tabs.update({ url: githubUrl })
+  })
 })
